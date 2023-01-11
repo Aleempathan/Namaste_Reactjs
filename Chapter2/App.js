c80e59fdc6aad793c2998bd90eb0db06d8411383
Aleem Nasiyathulla <aleemnasiyathulla2020@gmail.com>
2023-01-12 01:02:56 +0530

    chapter2 complete

---
+++ Chapter2/App.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1= React.createElement('h2',{id:'header1'},'Namaste React 🙏')
+
+const heading2= React.createElement('h3',{class:'heading2'},"Hello World , Welcome to React")
+
+const container = React.createElement('div',{class:'container'},[heading1,heading2])
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(container)
