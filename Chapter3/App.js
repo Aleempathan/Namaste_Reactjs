899b70891af3bcfe5c50c21763d7393a9e214c70
Aleem Nasiyathulla <aleemnasiyathulla2020@gmail.com>
2023-03-07 05:04:47 +0530

    chapter3

--- Chapter3/App.js
+++ Chapter3/App.js
@@ -1,12 +1,79 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from './assets/logo.png';
 
-const heading1= React.createElement('h2',{id:'header1'},'Namaste React 🙏')
+//  Nested header element with h1,h2,h3 using React.createElement()
+const header1 = React.createElement("h1", { id: "header1" }, "Learning react ");
+const header2 = React.createElement(
+  "h2",
+  { id: "header2" },
+  "This Chapters is explain about laying the foundation"
+);
+const header3 = React.createElement(
+  "h3",
+  { id: "header3" },
+  "Dive deep information about jsx"
+);
+const container = React.createElement("div", { id: "contianer" }, [
+  header1,
+  header2,
+  header3,
+]);
 
-const heading2= React.createElement('h3',{class:'heading2'},"Hello World , Welcome to React")
+// Nested header element by using jsx like h1,h2,h3 with container
+const header_element = (
+  <div id="contianer" className="container_card">
+    <h1>Learning react with 🙏 React</h1>
+    <h2>This Chapter completely aboyt laying out foundation</h2>
+    <h3>Dive Deep information about JSX</h3>
+  </div>
+);
 
-const container = React.createElement('div',{class:'container'},[heading1,heading2])
+//  Nested header element with h1,h2,h3 using JSX React Element and pass attributes
+const header_with_attr = (
+  <div id="container" className="contianer_card">
+    <h1>
+      Learning react with{" "}
+      <a href="https://learn.namastedev.com">🙏 React Course</a>{" "}
+    </h1>
+    <h2 className="header2">
+      <strong>Chapter:3 </strong>
+      This Chapter completely aboyt laying out foundation
+    </h2>
+    <h3>Dive Deep information about JSX</h3>
+  </div>
+);
 
- const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(container)
+// Nested header element with h1,h2,h3 using Functional Component
+const Title = () => (
+  <div id="container_title" className="contianer_card">
+    <h1>
+      Learning react with{" "}
+      <a href="https://learn.namastedev.com">🙏 React Course</a>{" "}
+    </h1>
+    <h2 className="header2">
+      <strong>Chapter:3 </strong> This Chapter completely aboyt laying out
+      foundation
+    </h2>
+    <h3>Dive Deep information about JSX</h3>
+  </div>
+);
+
+const Header = () => (
+  <div className="header_style">
+    <img src={logo} alt="brand-logo" width={80} />
+    <input type="text" placeholder="search" className="input_control"  />
+    <i class="fas fa-user user_icon" ></i>
+  </div>
+);
+//  Composition Component
+const Nested = () => (
+  <div>
+    <Header />
+    <Title />
+    <Title ></Title>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Nested />);
