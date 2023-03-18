bb97c384b95377e09e4c375336f567fc1cbfdf98
Aleem Nasiyathulla <aleemnasiyathulla2020@gmail.com>
2023-03-18 07:50:46 +0530

    login button conditional

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,33 @@
+import { useState } from "react";
 import logo from "../assets/logo.png";
 
-const Brandlogo =()=>(
-    <img src={logo} alt="brandlogo" width={100} />
-    )
-    
-    const Navlist = () =>(
-      <ul>
-        <li>Home</li>
-        <li>Offers</li>
-        <li>Sign In</li>
-        <li>Cart</li>
-      </ul>
-    )
-    
+const Brandlogo = () => <img src={logo} alt="brandlogo" width={100} />;
 
-const Header = () =>(
+const Navlist = () => (
+  <ul>
+    <li>Home</li>
+    <li>Offers</li>
+    <li>Sign In</li>
+    <li>Cart</li>
+  </ul>
+);
+
+const Header = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
     <>
-    <div className="Header_style">
-    <Brandlogo />
-    <Navlist />
-    </div>
+      <div className="Header_style">
+        <Brandlogo />
+        <Navlist />
+        {loggedIn ? (
+          <button onClick={() => setLoggedIn(false)}>LOGIN</button>
+        ) : (
+          <button onClick={() => setLoggedIn(true)}>LOGOUT</button>
+        )}
+      </div>
     </>
-  )
+  );
+};
 
-  export default Header;
+export default Header;
